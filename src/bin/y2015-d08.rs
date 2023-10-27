bd426813d335251cf7919dd46dce39d9111c4b99
Meadowsys <blazeykirin@gmail.com>
2023-10-27 05:48:02 -0700

    y2015 d08 pt1
    my friend silk rose said it looks easy, so, here we go lel

---
+++ src/bin/y2015-d08.rs
@@ -0,0 +1,24 @@
+use fancy_regex::Regex;
+
+fn main() {
+	let input_str = aoc::get_input!();
+
+	let code_size = input_str.trim()
+		.split('\n')
+		.map(str::len)
+		.sum::<usize>();
+
+	let escapes_regex = Regex::new("(?:\\\\\\\\|\\\\\"|\\\\x[0-9a-fA-F]{2})").unwrap();
+	let mem_size = input_str.trim()
+		.split('\n')
+		.map(|s| &s[1..s.len() - 1])
+		.map(|s| escapes_regex.replace_all(s, "b"))
+		.map(|s| s.len())
+		.sum::<usize>();
+
+	let difference = code_size - mem_size;
+
+	println!("part 1: code size: {code_size}");
+	println!("        memory size: {mem_size}");
+	println!("        difference: {difference}");
+}
