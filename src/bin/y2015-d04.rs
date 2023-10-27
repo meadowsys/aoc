f434e662ead4aed177546f603a38eba02b7b4ee5
Meadowsys <blazeykirin@gmail.com>
2023-10-27 04:22:54 -0700

    y2015 d04 pt2

--- src/bin/y2015-d04.rs
+++ src/bin/y2015-d04.rs
@@ -4,17 +4,28 @@ fn main() {
 	let input_str = aoc::get_input!();
 	let key = input_str.trim();
 
+	let (i, result) = get_first_hash_starting_with(key, "00000");
+	println!("part 1: first hash with 5 starting zeros: {i} ({result})");
+
+	let (i, result) = get_first_hash_starting_with(key, "000000");
+	println!("part 2: first hash with 6 starting zeros: {i} ({result})");
+}
+
+fn get_first_hash_starting_with(key: &str, starting_with: &str) -> (usize, String){
 	for i in 0usize.. {
 		let hash_input = format!("{key}{i}");
 
 		let mut hasher = Md5::new();
 		hasher.update(hash_input);
+
 		let result = Into::<[u8; 16]>::into(hasher.finalize())
 			.map(|e| format!("{e:02x?}"))
 			.join("");
-		if result.starts_with("00000") {
-			println!("part 1: first hash with 5 starting zeros: {i} ({result})");
-			break
+
+		if result.starts_with(starting_with) {
+			return (i, result)
 		}
 	}
+
+	unreachable!()
 }
