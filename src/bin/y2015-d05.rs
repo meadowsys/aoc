9209838203becb6359bbe7130c065cebd4a5d8c2
Meadowsys <blazeykirin@gmail.com>
2023-10-27 05:28:13 -0700

    must, consistent

--- src/bin/y2015-d05.rs
+++ src/bin/y2015-d05.rs
@@ -1,13 +1,13 @@
 use fancy_regex::Regex;
 
 fn main() {
-	let input = aoc::get_input!();
+	let input_str = aoc::get_input!();
 
 	let regex1_3_vowels = Regex::new("^(?:[a-zA-Z]*?[aeiouAEIOU]){3,}?[a-zA-Z]*?$").unwrap();
 	let regex2_double_letter = Regex::new("^[a-zA-Z]*?([a-zA-Z])\\1[a-zA-Z]*?$").unwrap();
 	let regex3_no_forbidden_sequences = Regex::new("^((?!ab|cd|pq|xy).)*$").unwrap();
 
-	let nice_strings_count = input.split('\n')
+	let nice_strings_count = input_str.split('\n')
 		.filter(|s| {
 			let r1 = regex1_3_vowels.is_match(s).unwrap();
 			let r2 = regex2_double_letter.is_match(s).unwrap();
@@ -22,7 +22,7 @@ fn main() {
 	let regex4_nonoverlapping_letter_pairs = Regex::new("^[a-zA-Z]*?([a-zA-Z][a-zA-Z])[a-zA-Z]*?\\1[a-zA-Z]*?$").unwrap();
 	let regex5_letter_repeat_with_letter_between = Regex::new("^[a-zA-Z]*?(?:([a-zA-Z])[a-zA-Z]\\1)[a-zA-Z]*?$").unwrap();
 
-	let nice_strings_count = input.split('\n')
+	let nice_strings_count = input_str.split('\n')
 		.filter(|s| {
 			let r1 = regex4_nonoverlapping_letter_pairs.is_match(s).unwrap();
 			let r2 = regex5_letter_repeat_with_letter_between.is_match(s).unwrap();
