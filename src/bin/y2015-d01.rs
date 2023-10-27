fac4ebd7626944fb81df90053ab31122e1f5f6da
Meadowsys <blazeykirin@gmail.com>
2023-10-27 00:12:42 -0700

    y2015 d01 pt2

--- src/bin/y2015-d01.rs
+++ src/bin/y2015-d01.rs
@@ -2,13 +2,25 @@ fn main() {
 	let input_str = aoc::get_input!();
 	let input_str = input_str.trim();
 
-	let floor = input_str.chars().fold(0isize, |acc, curr| {
-		match curr {
-			'(' => { acc + 1 }
-			')' => { acc - 1 }
-			_ => { unreachable!() }
-		}
-	});
+	let floor = input_str.chars()
+		.fold(0isize, |acc, curr| acc + get_increment(curr));
 
-	println!("floor {floor}");
+	println!("part 1: floor {floor}");
+
+	let mut current_floor = 0isize;
+	for (direction, i) in input_str.chars().zip(1..) {
+		current_floor += get_increment(direction);
+		if current_floor == -1 {
+			println!("part 2: step {i}");
+			break
+		}
+	}
+}
+
+fn get_increment(c: char) -> isize {
+	match c {
+		'(' => { 1 }
+		')' => { -1 }
+		_ => { unreachable!() }
+	}
 }
