e292b6b660167bc821d6bb5366f11b3ad05e884e
Meadowsys <blazeykirin@gmail.com>
2023-10-27 06:03:51 -0700

    y2015 d08 pt2

--- src/bin/y2015-d08.rs
+++ src/bin/y2015-d08.rs
@@ -8,17 +8,30 @@ fn main() {
 		.map(str::len)
 		.sum::<usize>();
 
-	let escapes_regex = Regex::new("\\\\\\\\|\\\\\"|\\\\x[0-9a-fA-F]{2}").unwrap();
+	let regex_to_unescape = Regex::new("\\\\\\\\|\\\\\"|\\\\x[0-9a-fA-F]{2}").unwrap();
 	let mem_size = input_str.trim()
 		.split('\n')
 		.map(|s| &s[1..s.len() - 1])
-		.map(|s| escapes_regex.replace_all(s, "b"))
+		.map(|s| regex_to_unescape.replace_all(s, "b"))
 		.map(|s| s.len())
 		.sum::<usize>();
 
 	let difference = code_size - mem_size;
 
 	println!("part 1: code size: {code_size}");
+	println!("        memory size: {mem_size}");
+	println!("        difference: {difference}");
+
+	let to_escape_regex = Regex::new("(?<uwu>\\\\|\")").unwrap();
+	let mem_size = input_str.trim()
+		.split('\n')
+		.map(|s| to_escape_regex.replace_all(s, "\\$uwu"))
+		.map(|s| s.len() + 2)
+		.sum::<usize>();
+
+	let difference = mem_size - code_size;
+
+	println!("part 2: code size: {code_size}");
 	println!("        memory size: {mem_size}");
 	println!("        difference: {difference}");
 }
