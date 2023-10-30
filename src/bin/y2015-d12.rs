958b14b27fd4427e2e42fbc022efcd7075be25b0
Meadowsys <blazeykirin@gmail.com>
2023-10-30 00:32:38 -0700

    y2015 d12 pt2

--- src/bin/y2015-d12.rs
+++ src/bin/y2015-d12.rs
@@ -5,24 +5,31 @@ fn main() {
 
 	let value = serde_json::from_str::<Value>(&input_str).unwrap();
 
-	let sum = process_value_sum(value);
+	let sum = process_value_sum(&value, false);
 	println!("part 1: sum of all numbers: {sum}");
+
+	let sum = process_value_sum(&value, true);
+	println!("part 2: sum, filtering objs with value \"red\": {sum}");
 }
 
 
-fn process_value_sum(value: Value) -> isize {
+fn process_value_sum(value: &Value, filter_red: bool) -> isize {
 	match value {
 		Value::Number(num) => { num.as_i64().unwrap() as isize }
 		Value::Array(array) => {
-			array.into_iter()
-				.map(process_value_sum)
+			array.iter()
+				.map(|i| process_value_sum(i, filter_red))
 				.sum()
 		}
 		Value::Object(obj) => {
-			obj.into_iter()
-				.map(|(_, val)| val)
-				.map(process_value_sum)
-				.sum()
+			if filter_red && obj.values().any(|v| if let Value::String(s) = v { s == "red" } else { false }) {
+				0
+			} else {
+				obj.iter()
+					.map(|(_, val)| val)
+					.map(|i| process_value_sum(i, filter_red))
+					.sum()
+			}
 		}
 
 		Value::Null | Value::Bool(_) | Value::String(_) => { 0 }
