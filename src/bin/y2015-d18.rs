4ae7a053eaf0320401366db4f76c4ebf33db7112
Meadowsys <blazeykirin@gmail.com>
2023-10-30 05:31:28 -0700

    y2015 d18 pt2

--- src/bin/y2015-d18.rs
+++ src/bin/y2015-d18.rs
@@ -19,15 +19,21 @@ fn main() {
 
 	let num_iterations = 100usize;
 
-	let mut new_coords = coords;
+	let mut new_coords = coords.clone();
 	for _ in 0..num_iterations {
-		new_coords = step(&new_coords, bounds);
+		new_coords = step(&new_coords, bounds, false);
 	}
-
 	println!("part 1: number of lights on: {}", new_coords.len());
+
+	let mut new_coords = coords.clone();
+	force_corners_on(&mut new_coords, bounds);
+	for _ in 0..num_iterations {
+		new_coords = step(&new_coords, bounds, true);
+	}
+	println!("part 2: number of lights on (corners forced on): {}", new_coords.len());
 }
 
-fn step(map: &aoc::Set<(isize, isize)>, (x_bound, y_bound): (isize, isize)) -> aoc::Set<(isize, isize)> {
+fn step(map: &aoc::Set<(isize, isize)>, (x_bound, y_bound): (isize, isize), should_force_corners_on: bool) -> aoc::Set<(isize, isize)> {
 	let mut next = aoc::set!();
 	let is_considered_on = |x, y| {
 		if x < 0 || y < 0 || x >= x_bound || y >= y_bound {
@@ -65,5 +71,13 @@ fn step(map: &aoc::Set<(isize, isize)>, (x_bound, y_bound): (isize, isize)) -> a
 		}
 	}
 
+	if should_force_corners_on { force_corners_on(&mut next, (x_bound, y_bound)) }
 	next
+}
+
+fn force_corners_on(map: &mut aoc::Set<(isize, isize)>, (x_bound, y_bound): (isize, isize)) {
+	map.insert((0, 0));
+	map.insert((x_bound - 1, 0));
+	map.insert((0, y_bound - 1));
+	map.insert((x_bound - 1, y_bound - 1));
 }
