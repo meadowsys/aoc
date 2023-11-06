2a671c33bb2be19579eab22b77a8da66659e19f6
Meadowsys <blazeykirin@gmail.com>
2023-11-06 01:26:53 -0800

    basic multithread for y2015 d20
    (nothing more than send part 1 and part 2 to their own threads then join for results, it still takes like 5m20s on my machine to run it all)

--- rust/src/bin/y2015-d20.rs
+++ rust/src/bin/y2015-d20.rs
@@ -1,3 +1,5 @@
+use std::thread::spawn;
+
 fn main() {
 	let input_str = aoc::get_input!();
 	let input = input_str.trim();
@@ -5,33 +7,37 @@ fn main() {
 	let target = input.parse::<usize>().unwrap();
 	let max_house = target / 10;
 
-	// let house = (1usize..=max_house)
-	// 	.find(|house| {
-	// 		let presents = (1..=*house)
-	// 			.map(|num| elf(num).presents_for(*house))
-	// 			.sum::<usize>();
+	let part1 = spawn(move || {
+		(1usize..=max_house)
+			.find(|house| {
+				let presents = (1..=*house)
+					.map(|num| elf(num).presents_for(*house))
+					.sum::<usize>();
 
-	// 		presents >= target
-	// 	})
-	// 	.unwrap_or(max_house);
-
-	// println!("part 1: house with at least {input} presents: {house}");
+				presents >= target
+			})
+			.unwrap_or(max_house)
+	});
 
 	let max_house = (target as f64 / 11.).ceil() as usize;
 
-	let house = (1usize..=max_house)
-		.find(|house| {
-			let presents = (1..=*house)
-				.map(|num| elf(num).lazy_presents_for(*house))
-				.sum::<usize>();
+	let part2 = spawn(move || {
+		(1usize..=max_house)
+			.find(|house| {
+				let presents = (1..=*house)
+					.map(|num| elf(num).lazy_presents_for(*house))
+					.sum::<usize>();
 
-			presents >= target
-		})
-		.unwrap_or(max_house);
+				presents >= target
+			})
+			.unwrap_or(max_house)
+	});
 
-	println!("part 2: house with at least {input} presents (elfs are lazy): {house}");
+	let part1 = part1.join().unwrap();
+	let part2 = part2.join().unwrap();
 
-
+	println!("part 1: house with at least {input} presents: {part1}");
+	println!("part 2: house with at least {input} presents (elfs are lazy): {part2}");
 }
 
 #[repr(transparent)]
