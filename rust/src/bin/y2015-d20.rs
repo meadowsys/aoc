f1338852208cb0acea9047d9b14b4e37e27fdeab
Meadowsys <blazeykirin@gmail.com>
2023-11-06 01:16:37 -0800

    y2015 d20 pt1
    its not just me who has a slow solution, this problem is just a slow runner apparently?

--- rust/src/bin/y2015-d20.rs
+++ rust/src/bin/y2015-d20.rs
@@ -5,17 +5,33 @@ fn main() {
 	let target = input.parse::<usize>().unwrap();
 	let max_house = target / 10;
 
+	// let house = (1usize..=max_house)
+	// 	.find(|house| {
+	// 		let presents = (1..=*house)
+	// 			.map(|num| elf(num).presents_for(*house))
+	// 			.sum::<usize>();
+
+	// 		presents >= target
+	// 	})
+	// 	.unwrap_or(max_house);
+
+	// println!("part 1: house with at least {input} presents: {house}");
+
+	let max_house = (target as f64 / 11.).ceil() as usize;
+
 	let house = (1usize..=max_house)
 		.find(|house| {
 			let presents = (1..=*house)
-				.map(|num| elf(num).presents_for(*house))
+				.map(|num| elf(num).lazy_presents_for(*house))
 				.sum::<usize>();
 
 			presents >= target
 		})
 		.unwrap_or(max_house);
 
-	println!("part 1: house with at least {input} presents: {house}");
+	println!("part 2: house with at least {input} presents (elfs are lazy): {house}");
+
+
 }
 
 #[repr(transparent)]
@@ -31,6 +47,18 @@ impl Elf {
 	fn presents_for(self, house: usize) -> usize {
 		if house % self.0 == 0 {
 			self.0 * 10
+		} else {
+			0
+		}
+	}
+
+	#[inline]
+	fn lazy_presents_for(self, house: usize) -> usize {
+		// let past_max = (house / self.0) > 50;
+		let still_not_lazy = (50 * self.0) >= house;
+
+		if still_not_lazy && house % self.0 == 0 {
+			self.0 * 11
 		} else {
 			0
 		}
