f4f2383e1624a9e940ceba32601c2052ae8d9614
Meadowsys <blazeykirin@gmail.com>
2023-11-07 03:12:50 -0800

    y2016 d03 pt2

--- rust/2016/src/bin/y2016-d03.rs
+++ rust/2016/src/bin/y2016-d03.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools as _;
 use pest::Parser as _;
 
 fn main() {
@@ -8,13 +9,49 @@ fn main() {
 	let file = input.next().unwrap();
 	assert!(matches!(file.as_rule(), Rule::file));
 
-	let valid_triangles = file.into_inner()
-		.take_while(|l| !matches!(l.as_rule(), Rule::EOI))
+	let lines = file.into_inner()
+		.take_while(|l| !matches!(l.as_rule(), Rule::EOI));
+
+	let valid_triangles = lines.clone()
 		.map(|l| l.into_triangle())
 		.filter(|t| t.is_valid())
 		.collect::<Vec<_>>();
 
 	println!("part 1: number of valid triangles: {}", valid_triangles.len());
+
+	let valid_triangles = lines.clone()
+		.batching(|iter| match iter.next() {
+			None => { None }
+			Some(iter1) => {
+				let iter1 = iter1.into_inner();
+				let iter2 = iter.next().unwrap().into_inner();
+				let iter3 = iter.next().unwrap().into_inner();
+
+				let iter = iter1.zip(iter2).zip(iter3);
+
+				let triangles = iter.take(3)
+					.map(|((s1, s2), s3)| {
+						let s1 = s1.as_str().parse::<usize>().unwrap();
+						let s2 = s2.as_str().parse::<usize>().unwrap();
+						let s3 = s3.as_str().parse::<usize>().unwrap();
+
+						let mut sorter = [s1, s2, s3];
+						sorter.sort_unstable();
+						let [s1, s2, s3] = sorter;
+
+						(s1, s2, s3)
+					})
+					.collect::<Vec<_>>();
+
+				Some(triangles)
+			}
+		})
+		.flatten()
+		.map(|(s1, s2, s3)| Triangle(s1, s2, s3))
+		.filter(|t| t.is_valid())
+		.collect::<Vec<_>>();
+
+	println!("part 2: number of valid triangles when col: {}", valid_triangles.len());
 }
 
 #[derive(pest_derive::Parser)]
