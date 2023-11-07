0b038475309f5a862f4d6f524e5e71256558e540
Meadowsys <blazeykirin@gmail.com>
2023-11-07 02:35:11 -0800

    y2016 d02 pt2

--- rust/2016/src/bin/y2016-d02.rs
+++ rust/2016/src/bin/y2016-d02.rs
@@ -6,57 +6,137 @@ fn main() {
 		.map(|l| l.trim())
 		.collect::<Vec<_>>();
 
+	let code = process_lines_with(&lines, map_char_keypad_normal);
+	println!("part 1: the code: {code}");
+
+	let code = process_lines_with(&lines, map_char_keypad_weird);
+	println!("part 1: the code with weird keypad: {code}");
+}
+
+fn process_lines_with(lines: &[&str], f: impl Fn(u8, u8) -> u8) -> String {
 	let mut code = String::with_capacity(lines.len());
-	let mut previous_digit = 5;
+	let mut previous_digit = b'5';
 
 	for line in lines {
 		let mut current = previous_digit;
 
 		for direction in line.chars() {
-			current = map_char(current, direction as u8);
+			current = f(current, direction as u8);
 		}
 
-		code.push_str(&current.to_string());
+		code.push(current as char);
 		previous_digit = current;
 	}
 
-	println!("part 1: the code: {code}");
+	code
 }
 
 #[allow(clippy::manual_range_patterns)]
-fn map_char(digit: u8, direction: u8) -> u8 {
+fn map_char_keypad_normal(digit: u8, direction: u8) -> u8 {
 	match direction {
 		b'U' => match digit {
-			1 | 2 | 3 => { digit }
-			4 | 5 | 6 |
-			7 | 8 | 9
+			b'1' | b'2' | b'3' => { digit }
+			b'4' | b'5' | b'6' |
+			b'7' | b'8' | b'9'
 			=> { digit - 3 }
 			_ => { unreachable!() }
 		}
 
 		b'R' => match digit {
-			3 | 6 | 9 => { digit }
-			1 | 2 |
-			4 | 5 |
-			7 | 8
+			b'3' | b'6' | b'9' => { digit }
+			b'1' | b'2' |
+			b'4' | b'5' |
+			b'7' | b'8'
 			=> { digit + 1 }
 			_ => { unreachable!() }
 		}
 
 		b'D' => match digit {
-			7 | 8 | 9 => { digit }
-			1 | 2 | 3 |
-			4 | 5 | 6
+			b'7' | b'8' | b'9' => { digit }
+			b'1' | b'2' | b'3' |
+			b'4' | b'5' | b'6'
 			=> { digit + 3 }
 			_ => { unreachable!() }
 		}
 
 		b'L' => match digit {
-			1 | 4 | 7 => { digit }
-			2 | 3 |
-			5 | 6 |
-			8 | 9
+			b'1' | b'4' | b'7' => { digit }
+			b'2' | b'3' |
+			b'5' | b'6' |
+			b'8' | b'9'
 			=> { digit - 1 }
+			_ => { unreachable!() }
+		}
+
+		_ => { unreachable!() }
+	}
+}
+
+#[allow(clippy::manual_range_patterns)]
+fn map_char_keypad_weird(digit: u8, direction: u8) -> u8 {
+	match direction {
+		b'U' => match digit {
+			b'1' => { b'1' }
+			b'2' => { b'2' }
+			b'3' => { b'1' }
+			b'4' => { b'4' }
+			b'5' => { b'5' }
+			b'6' => { b'2' }
+			b'7' => { b'3' }
+			b'8' => { b'4' }
+			b'9' => { b'9' }
+			b'A' => { b'6' }
+			b'B' => { b'7' }
+			b'C' => { b'8' }
+			b'D' => { b'B' }
+			_ => { unreachable!() }
+		}
+		b'R' => match digit {
+			b'1' => { b'1' }
+			b'2' => { b'3' }
+			b'3' => { b'4' }
+			b'4' => { b'4' }
+			b'5' => { b'6' }
+			b'6' => { b'7' }
+			b'7' => { b'8' }
+			b'8' => { b'9' }
+			b'9' => { b'9' }
+			b'A' => { b'B' }
+			b'B' => { b'C' }
+			b'C' => { b'C' }
+			b'D' => { b'D' }
+			_ => { unreachable!() }
+		}
+		b'D' => match digit {
+			b'1' => { b'3' }
+			b'2' => { b'6' }
+			b'3' => { b'7' }
+			b'4' => { b'8' }
+			b'5' => { b'5' }
+			b'6' => { b'A' }
+			b'7' => { b'B' }
+			b'8' => { b'C' }
+			b'9' => { b'9' }
+			b'A' => { b'A' }
+			b'B' => { b'D' }
+			b'C' => { b'C' }
+			b'D' => { b'D' }
+			_ => { unreachable!() }
+		}
+		b'L' => match digit {
+			b'1' => { b'1' }
+			b'2' => { b'2' }
+			b'3' => { b'2' }
+			b'4' => { b'3' }
+			b'5' => { b'5' }
+			b'6' => { b'5' }
+			b'7' => { b'6' }
+			b'8' => { b'7' }
+			b'9' => { b'8' }
+			b'A' => { b'A' }
+			b'B' => { b'A' }
+			b'C' => { b'B' }
+			b'D' => { b'D' }
 			_ => { unreachable!() }
 		}
 
