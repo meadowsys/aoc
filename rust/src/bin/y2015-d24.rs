5db180ab65a5e6ce3e63f9be3cbe9d47fc3719e4
Meadowsys <blazeykirin@gmail.com>
2023-11-07 00:06:18 -0800

    y2015 d24 pt2

--- rust/src/bin/y2015-d24.rs
+++ rust/src/bin/y2015-d24.rs
@@ -8,17 +8,27 @@ fn main() {
 		.map(|s| s.parse::<usize>().unwrap())
 		.collect::<Vec<_>>();
 	input.sort_unstable();
-	let weight_per_side = input.iter().sum::<usize>() / 3;
+
+	let smol = get_smol(&input, 3);
+	println!("part 1: smollest, ideal quantum entanglement: {smol}");
+
+	let smol = get_smol(&input, 4);
+	println!("part 2: smollest, ideal quantum entanglement, now with trunk: {smol}");
+}
+
+fn get_smol(input: &[usize], k: usize) -> usize {
+	let weight_per_side = input.iter().sum::<usize>() / k;
 
 	for i in 1..input.len() {
-		let small = input.iter()
+		let smol = input.iter()
 			.permutations(i)
 			.filter(|p| p.iter().copied().sum::<usize>() == weight_per_side)
 			.map(|p| p.iter().copied().product::<usize>())
 			.min();
-		if let Some(small) = small {
-			println!("part 1: smallest quantum entanglement in ideal arrangement: {small}");
-			break
+		if let Some(smol) = smol {
+			return smol
 		}
 	}
+
+	unreachable!("aoc input should be valid??");
 }
